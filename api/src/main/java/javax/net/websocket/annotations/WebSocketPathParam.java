f2496d41205776672fd3b5dba589fc498d51daa4
stepan.kopriva <stepan.kopriva@456f2b81-f124-cda4-d979-c1d8e3325ace>
2012-10-26 08:30:31 +0000

    Changed @Tarrget at @WebSocketPathParam to ElementType.PARAMETER.
    git-svn-id: https://svn.java.net/svn/websocket-spec~source-code-repository/trunk@35 456f2b81-f124-cda4-d979-c1d8e3325ace

--- api/src/main/java/javax/net/websocket/annotations/WebSocketPathParam.java
+++ api/src/main/java/javax/net/websocket/annotations/WebSocketPathParam.java
@@ -14,26 +14,26 @@ import java.lang.annotation.Target;
  * where a URI-template has been used in the path-mapping of the WebSocketEndpoint
  * annotation.<br> For example:-
  * * For example: <br><code><br>
- * 
+ *
  * &nbsp@WebSocketEndpoint("/bookings/{guest-id}");<br>
  * public class BookingServer {<br><br>
- * 
+ *
  * &nbsp&nbsp@WebSocketMessage<br>
  * &nbsppublic void processBookingRequest(@WebSocketPathParam("guest-id") String guestID, String message, Session session) {<br>
  * &nbsp&nbsp&nbsp// process booking from the given guest here<br>
  * &nbsp}<br>
  * }
  * </code>
- * 
+ *
  * @author dannycoward
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
+@Target(ElementType.PARAMETER)
 public @interface WebSocketPathParam {
     /** The name of the variable used in the URI-template. If the name does
      not match a path variable in the URI-template, the value of the method parameter
      this annotation annotates is null.
      @return the value */
     public String value();
-  
+
 }
