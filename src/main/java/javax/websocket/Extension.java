0ff9f12d086e12859b2ee03a5382fd4587414df4
dannycoward <dannycoward@456f2b81-f124-cda4-d979-c1d8e3325ace>
2013-01-28 19:37:01 +0000

    moved to ordered extension parameters. http://java.net/jira/browse/WEBSOCKET_SPEC-105
    git-svn-id: https://svn.java.net/svn/websocket-spec~source-code-repository/trunk/api@119 456f2b81-f124-cda4-d979-c1d8e3325ace

--- src/main/java/javax/websocket/Extension.java
+++ src/main/java/javax/websocket/Extension.java
@@ -4,7 +4,7 @@
  */
 package javax.websocket;
 
-import java.util.Map;
+import java.util.List;
 
 /**
  * A simple representation of a websocket extension as a name and map of extension parameters.
@@ -21,9 +21,26 @@ public interface Extension {
     String getName();
 
     /**
-     * The extension parameters for this extension.
+     * The extension parameters for this extension in the order
+     * they appear in the http headers.
      *
      * @return The read-only Map of extension parameters belonging to this extension.
      */
-    Map<String, String> getParameters();
+    List<Parameter> getParameters();
+    
+    /**
+     * This member interface defines a single websocket extension parameter.
+     */
+    interface Parameter {
+        /** 
+         * Return the name of the extension parameter. 
+         * @return the name of the parameter.
+         */
+        String getName();
+        /**
+         * Return the value of the extension parameter.
+         * @return the value of the parameter.
+         */
+        String getValue();
+    }
 }
