b29741fc2004ed0da907e7e4217927fc4ceec137
dannycoward <dannycoward@456f2b81-f124-cda4-d979-c1d8e3325ace>
2013-01-31 21:48:50 +0000

    Javadoc clarifications for http://java.net/jira/browse/WEBSOCKET_SPEC-88
    git-svn-id: https://svn.java.net/svn/websocket-spec~source-code-repository/trunk@120 456f2b81-f124-cda4-d979-c1d8e3325ace

--- api/src/main/java/javax/websocket/CloseReason.java
+++ api/src/main/java/javax/websocket/CloseReason.java
@@ -39,6 +39,8 @@
  */
 package javax.websocket;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * A class encapsulating the reason why a web socket has been closed, or why it is being asked to
  * close. Note the acceptable uses of codes and reason phrase defined in FRC 6455.
@@ -48,8 +50,8 @@ package javax.websocket;
  */
 public class CloseReason {
 
-    private CloseReason.CloseCode closeCode;
-    private String reasonPhrase;
+    private final CloseReason.CloseCode closeCode;
+    private final String reasonPhrase;
 
     /**
      * Creates a reason for closing a web socket connection with the given
@@ -59,6 +61,13 @@ public class CloseReason {
      * @param reasonPhrase the reason phrase
      */
     public CloseReason(CloseReason.CloseCode closeCode, String reasonPhrase) {
+        try {
+            if (reasonPhrase.getBytes("UTF-8").length > 123) {
+                throw new IllegalArgumentException("Reason Phrase cannot exceed 123 UTF-8 encoded bytes: " + reasonPhrase);
+            }
+        } catch (UnsupportedEncodingException uee) {
+            throw new IllegalStateException(uee);
+        }
         this.closeCode = closeCode;
         this.reasonPhrase = reasonPhrase;
     }
@@ -104,35 +113,91 @@ public class CloseReason {
      * are defined in the specification.
      */
     public enum CloseCodes implements CloseReason.CloseCode {
-        /* 1000 */
+        /** 
+         * 1000 indicates a normal closure, meaning that the purpose for
+         * which the connection was established has been fulfilled.
+         */
         NORMAL_CLOSURE(1000),
-        /* 1001 */
+        /**
+         * 1001 indicates that an endpoint is "going away", such as a server
+         * going down or a browser having navigated away from a page.
+         */
         GOING_AWAY(1001),
-        /* 1002 */
+        /**
+         * 1002 indicates that an endpoint is terminating the connection due
+         * to a protocol error.
+         */
         PROTOCOL_ERROR(1002),
-        /* 1003 */
+        /** 
+         * 1003 indicates that an endpoint is terminating the connection
+         * because it has received a type of data it cannot accept (e.g., an
+         * endpoint that understands only text data MAY send this if it
+         * receives a binary message). 
+         */
         CANNOT_ACCEPT(1003),
-        /* 1004 */
-        RESERVED(1004
-                /* 1005 */),
+        /**
+         * Reserved.  The specific meaning might be defined in the future.
+         */
+        RESERVED(1004),
+        /** 
+         * 1005 is a reserved value and MUST NOT be set as a status code in a
+         * Close control frame by an endpoint.  It is designated for use in
+         * applications expecting a status code to indicate that no status
+         * code was actually present.
+         */
         NO_STATUS_CODE(1005),
-        /* 1006 */
+        /**
+         * 1006 is a reserved value and MUST NOT be set as a status code in a
+         * Close control frame by an endpoint.  It is designated for use in
+         * applications expecting a status code to indicate that the
+         * connection was closed abnormally, e.g., without sending or
+         * receiving a Close control frame. 
+         */
         CLOSED_ABNORMALLY(1006),
-        /* 1007 */
+        /**
+         * 1007 indicates that an endpoint is terminating the connection
+         * because it has received data within a message that was not
+         * consistent with the type of the message (e.g., non-UTF-8 
+         * data within a text message).
+         */
         NOT_CONSISTENT(1007),
-        /* 1008 */
+        /**
+         * 1008 indicates that an endpoint is terminating the connection
+         * because it has received a message that violates its policy.  This
+         * is a generic status code that can be returned when there is no
+         * other more suitable status code (e.g., 1003 or 1009) or if there
+         * is a need to hide specific details about the policy.
+         */
         VIOLATED_POLICY(1008),
-        /* 1009 */
+        /** 
+         * 1009 indicates that an endpoint is terminating the connection
+         * because it has received a message that is too big for it to
+         * process.
+         */
         TOO_BIG(1009),
-        /* 101 */
+        /**
+        * 1010 indicates that an endpoint (client) is terminating the
+        * connection because it has expected the server to negotiate one or
+        * more extension, but the server didn't return them in the response
+        * message of the WebSocket handshake.  The list of extensions that
+        * are needed SHOULD appear in the /reason/ part of the Close frame.
+        * Note that this status code is not used by the server, because it
+        * can fail the WebSocket handshake instead.
+        */
         NO_EXTENSION(1010),
-        /* 1011 */
+        /**
+         * 1011 indicates that a server is terminating the connection because
+         * it encountered an unexpected condition that prevented it from
+         * fulfilling the request. 
+         */
         UNEXPECTED_CONDITION(1011),
-        /* 1012 */
-        SERVICE_RESTART(1012),
-        /* 1013 */
-        TRY_AGAIN_LATER(1013),
-        /* 1015 */
+        /**
+         * 1015 is a reserved value and MUST NOT be set as a status code in a
+         * Close control frame by an endpoint.  It is designated for use in
+         * applications expecting a status code to indicate that the
+         * connection was closed due to a failure to perform a TLS handshake
+         * (e.g., the server certificate can't be verified).
+         */
         TLS_HANDSHAKE_FAILURE(1015);
 
 
