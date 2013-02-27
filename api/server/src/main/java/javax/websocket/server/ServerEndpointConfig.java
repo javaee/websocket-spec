c33a68dfdb2a2a1919f0c95f3b8b11ad73ffa3dc
dannycoward <dannycoward@456f2b81-f124-cda4-d979-c1d8e3325ace>
2013-02-27 22:32:55 +0000

    moved builder to member class, like Configurator
    git-svn-id: https://svn.java.net/svn/websocket-spec~source-code-repository/trunk@240 456f2b81-f124-cda4-d979-c1d8e3325ace

--- api/server/src/main/java/javax/websocket/server/ServerEndpointConfig.java
+++ api/server/src/main/java/javax/websocket/server/ServerEndpointConfig.java
@@ -40,9 +40,13 @@
 package javax.websocket.server;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
+import javax.websocket.Decoder;
+import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
 import javax.websocket.Extension;
 import javax.websocket.HandshakeResponse;
@@ -51,7 +55,8 @@ import javax.websocket.HandshakeResponse;
  * The ServerEndpointConfig is a special kind of endpoint configuration object that contains
  * web socket configuration information specific only to server endpoints. For developers
  * deploying programmatic endpoints, ServerEndpointConfig objects
- * can be created using a {@link ServerEndpointConfigBuilder}.
+ * can be created using a {@link ServerEndpointConfig.Builder}. Certain configuration
+ * operations can be customized by providing a {@link ServerEndpointConfig.Configurator}
  *
  * @author dannycoward
  * @since DRAFT 001
@@ -282,5 +287,147 @@ public interface ServerEndpointConfig extends EndpointConfig {
         } 
 
     }
+
+
+    /**
+     * The ServerEndpointConfig.Builder is a class used for creating
+     * {@link ServerEndpointConfig.Builder} objects for the purposes of
+     * deploying a client endpoint.
+     * <br><br>Here are some examples:<br>
+     * <br>Building a plain configuration for an endpoint with just a path.<br><br>
+     * <code>
+     * ServerEndpointConfig config = ServerEndpointConfig.Builder.create(ProgrammaticEndpoint.class, "/foo").build();<br>
+     * </code>
+     * 
+     * <br><br>Building a configuration with no subprotocols and a custom configurator.<br><br>
+     * <code>
+     * ServerEndpointConfig config = ServerEndpointConfig.Builder.create(ProgrammaticEndpoint.class, "/bar")<br>
+                    .subprotocols(subprotocols)<br>
+                    .configurator(new MyServerConfigurator())<br>
+                    .build();<br>
+     * </code>
+     * 
+     * @author dannycoward
+     */
+    public final class Builder {
+        private String path;
+        private Class endpointClass;
+        private List<String> subprotocols = new ArrayList<String>();
+        private List<Extension> extensions = new ArrayList<Extension>();
+        private List<Encoder> encoders = new ArrayList<Encoder>();
+        private List<Decoder> decoders = new ArrayList<Decoder>();
+        private ServerEndpointConfig.Configurator serverEndpointConfigurator;
+
+        /**
+         * Creates the builder with the mandatory information of the endpoint class 
+         * (programmatic or annotated), the relative URI or URI-template to use,
+         * and with no subprotocols, extensions, encoders, decoders or custom
+         * configurator.
+         * @param endpointClass the class of the endpoint to configure
+         * @param path The URI or URI template where the endpoint will be deployed.
+         * A trailing "/" will be ignored and the path must begin with /.
+         * @return a new instance of ServerEndpointConfig.Builder
+         */
+        public static Builder create(Class endpointClass, String path) {
+            return new Builder(endpointClass, path);
+        }
+
+        // only one way to build them
+        private Builder() {
+
+        }
+        /**
+         * Builds the configuration object using the current attributes
+         * that have been set on this builder object.
+         * 
+         * @return a new ServerEndpointConfig object.
+         */
+        public ServerEndpointConfig build() {
+            return new DefaultServerEndpointConfig(
+                    this.endpointClass,
+                    this.path,
+                    Collections.unmodifiableList(this.subprotocols),
+                    Collections.unmodifiableList(this.extensions),
+                    Collections.unmodifiableList(this.encoders),
+                    Collections.unmodifiableList(this.decoders),
+                    this.serverEndpointConfigurator
+                 );
+        }
+
+        private Builder(Class endpointClass, String path) {
+            if (endpointClass == null) {
+                throw new IllegalArgumentException("endpointClass cannot be null");
+            }
+            this.endpointClass = endpointClass;
+            if (path == null || !path.startsWith("/")) {
+                throw new IllegalStateException("Path cannot be null and must begin with /");
+            }
+            this.path = path;
+        }
+
+
+
+        /** 
+         * Sets the list of encoders for this builder.
+         * 
+         * @param encoders the encoders
+         * @return this builder instance
+         */
+        public ServerEndpointConfig.Builder encoders(List<Encoder> encoders) {
+            this.encoders = (encoders == null) ? new ArrayList<Encoder>() : encoders;
+            return this;
+        }
+
+
+        /**
+         * Sets the decoders to use in the configuration.
+         * 
+         * @param decoders the decoders
+         * @return this builder instance.
+         */
+        public ServerEndpointConfig.Builder decoders(List<Decoder> decoders) {
+            this.decoders = (decoders == null) ? new ArrayList<Decoder>() : decoders;
+            return this;
+        }
+
+        /**
+         * Sets the subprotocols to use in the configuration.
+         * 
+         * @param subprotocols the subprotocols.
+         * @return this builder instance
+         */
+        public ServerEndpointConfig.Builder subprotocols(List<String> subprotocols) {
+            this.subprotocols = (subprotocols == null) ? new ArrayList<String>() : subprotocols;
+            return this;
+        }
+
+
+        /**
+         * Sets the extensions to use in the configuration.
+         * 
+         * @param extensions the extensions to use.
+         * @return this builder instance.
+         */
+        public ServerEndpointConfig.Builder extensions(List<Extension> extensions) {
+            this.extensions = (extensions == null) ? new ArrayList<Extension>() : extensions;
+            return this;
+        }
+
+        /** 
+         * Sets the custom configurator to use on the configuration
+         * object built by this builder.
+         * 
+         * @param serverEndpointConfigurator the configurator
+         * @return this builder instance
+         */
+        public ServerEndpointConfig.Builder configurator(ServerEndpointConfig.Configurator serverEndpointConfigurator) {
+            this.serverEndpointConfigurator = serverEndpointConfigurator;
+            return this;
+        }
+
+
+
+    }
+
     
 }
