752d034f44282b6225883f907e90f2788ea6c02c
dannycoward <dannycoward@456f2b81-f124-cda4-d979-c1d8e3325ace>
2013-04-23 18:33:59 +0000

    clearer javadoc covering the custom configurator/default configurator cases better per issue: 187
    git-svn-id: https://svn.java.net/svn/websocket-spec~source-code-repository/trunk@342 456f2b81-f124-cda4-d979-c1d8e3325ace

--- api/client/src/main/java/javax/websocket/Endpoint.java
+++ api/client/src/main/java/javax/websocket/Endpoint.java
@@ -40,14 +40,35 @@
 package javax.websocket;
 
 
-/**
- * The Web Socket Endpoint represents an object that can handle a web socket conversations. When
- * deployed as a server endpoint, that is to say, the endpoint is registered to a URL, the server instantiates
- * a new endpoint instance for each client connection. If
- * deployed as a client, the endpoint will be instantiated once per single connection to the server.
- * If the endpoint is a server which will cater to multiple clients, each endpoint instance corresponding to each active client
- * is called by no more than one thread at a time. This means that when implementing/overriding the methods
- * of Endpoint, the developer is guaranteed that there will be at most one thread in each endpoint instance.
+/** 
+ * The Web Socket Endpoint represents an object that can handle websocket conversations. 
+ * Developers may extend this class in order to implement a programmatic websocket 
+ * endpoint. The Endpoint class holds lifecycle methods that may be 
+ * overridden to intercept websocket open, error and close events. By implementing 
+ * the {@link Endpoint#onOpen(javax.websocket.Session, javax.websocket.EndpointConfig) onOpen} method, the programmatic endpoint gains access to the {@link Session} object, 
+ * to which the developer may add {@link MessageHandler} implementations in order to 
+ * intercept incoming websocket messages. Each instance 
+ * of a websocket endpoint is guaranteed not to be called by more than one thread 
+ * at a time per active connection.
+ *
+ * <p>If deployed as a client endpoint, it will be instantiated once for the 
+ * single connection to the server. 
+ *
+ * <p>When deployed as a server endpoint, the implementation uses the 
+ * {@link javax.websocket.server.ServerEndpointConfig.Configurator#getEndpointInstance} 
+ * method to obtain the 
+ * endpoint instance it will use for each new client connection.  If the developer uses 
+ * the default {@link javax.websocket.server.ServerEndpointConfig.Configurator}, 
+ * there will be precisely one 
+ * endpoint instance per active client connection. Consequently, in this typical 
+ * case, when implementing/overriding the methods of Endpoint, the developer is 
+ * guaranteed that there will be at most one thread calling each endpoint instance
+ * at a time.
+ * 
+ * <p>If the developer provides a custom {@link javax.websocket.server.ServerEndpointConfig.Configurator}
+ * which overrides the default policy for endpoint instance creation, for example, 
+ * using a single Endpoint instance for multiple client connections, the developer
+ * may need to write code that can execute concurrently.
  *
  * <p>Here is an example of a simple endpoint that echoes any incoming text message back to the sender.
  * <pre><code>
